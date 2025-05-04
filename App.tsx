0c2b11150f1f49487a3fe804649b937912594274
Waqas Raza <waqasraza123@gmail.com>
2025-05-04 11:49:02 +0500

    tamagui config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider } from 'tamagui'
+import { config } from './tamagui.config'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <TamaguiProvider config={config}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TamaguiProvider>
+    </SafeAreaProvider >
   );
 }
 
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,14 +1,42 @@
-import { createTamagui } from 'tamagui'
-import { config as defaultConfig } from '@tamagui/config'
+import { createTamagui } from '@tamagui/core'
 
-const config = createTamagui({
-    ...defaultConfig,
+export const config = createTamagui({
+    tokens: {
+        size: { sm: 8, md: 12, lg: 20 },
+        space: { '-sm': 8 },
+        radius: { none: 0, sm: 3 },
+        color: { white: '#fff', black: '#000' },
+    },
+
+    themes: {
+        light: {
+            bg: '#f2f2f2',
+            color: '#fff',
+        },
+        dark: {
+            bg: '#111',
+            color: '#000',
+        },
+    },
+    media: {
+        sm: { maxWidth: 860 },
+        gtSm: { minWidth: 860 + 1 },
+        short: { maxHeight: 820 },
+        hoverNone: { hover: 'none' },
+        pointerCoarse: { pointer: 'coarse' },
+    },
+
+    shorthands: {
+        px: 'paddingHorizontal',
+    },
+
+    settings: {
+        disableSSR: true,
+        allowedStyleValues: 'somewhat-strict-web',
+    },
 })
 
-export type Conf = typeof config
-
-declare module 'tamagui' {
-    interface TamaguiCustomConfig extends Conf { }
+type AppConfig = typeof config
+declare module '@tamagui/core' {
+    interface TamaguiCustomConfig extends AppConfig { }
 }
-
-export default config
