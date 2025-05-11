a7cb8b78d9e04ad7691dcdcb66f398e2dcbb9188
Waqas Raza <waqasraza123@gmail.com>
2025-05-11 08:41:28 +0500

    react-native-reanimated updated

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname, { resetCache: true });
 config.resolver.unstable_enablePackageExports = false;
 
 module.exports = withNativeWind(config, { input: './src/styles/global.css' })
